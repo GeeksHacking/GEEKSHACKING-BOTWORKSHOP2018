2c0aa78e1ba4b0689f60c6fc42c4684dc107456d
jiachen <jiachen247@gmail.com>
2018-02-20 22:21:50 +0800

    fixed tabs

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 
-
 from telegram.ext import Updater
 
 TOKEN = "400458894:AAHBO9dzUTASfEWZEbOB0oaN3k1wLVg2niM"
@@ -12,28 +11,20 @@ WEBHOOK_URL ="https://{}.herokuapp.com/{}".format(APP_NAME, WEBHOOK_PATH)
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import escape_markdown
-import logging
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
 
 
 def start(bot, update):
-    user_name = update.effective_user.name
+	user_name = update.effective_user.name
     update.message.reply_text('Hello *{}*!'.format(escape_markdown(user_name))
 				,parse_mode=True)
 
 
 def help(bot, update):
-    update.message.reply_text('this is the list of commands i support!!! \n...')
+	update.message.reply_text('this is the list of commands i support!!! \n...')
 
 
 def message(bot, update):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+	update.message.reply_text(update.message.text)
 
 def main():
 
@@ -45,14 +36,12 @@ def main():
 
 	updater.bot.set_webhook(WEBHOOK_URL)
 
-    	dp = updater.dispatcher
+    dp = updater.dispatcher
 
-    	dp.add_handler(CommandHandler("start", start))
-    	dp.add_handler(CommandHandler("help", help))
-    	dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
-    	# log all errors
-    	dp.add_error_handler(error)
 	updater.idle()
 
 
