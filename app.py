87eb40959a6c1d960f186fda59ba529ee567ee41
jiachen <jiachen247@gmail.com>
2018-02-20 22:29:36 +0800

    ewsz

--- app.py
+++ app.py
@@ -45,16 +45,16 @@ def main():
 
 	updater.bot.set_webhook(WEBHOOK_URL)
 
-    	dp = updater.dispatcher
+	dp = updater.dispatcher
 
-    	dp.add_handler(CommandHandler("start", start ))
-    	dp.add_handler(CommandHandler("help", help))
-    	dp.add_handler(MessageHandler(Filters.text, echo))
+	dp.add_handler(CommandHandler("start", start ))
+	dp.add_handler(CommandHandler("help", help))
+	dp.add_handler(MessageHandler(Filters.text, echo))
 
-    	# log all errors
-    	dp.add_error_handler(error)
+	# log all errors
+	dp.add_error_handler(error)
 	updater.idle()
 
 
 if __name__ == '__main__':
-    main()
+	main()
