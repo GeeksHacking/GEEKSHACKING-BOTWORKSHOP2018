ff3e9171781e23d39d9b166faf2119e6e39e7e1d
jiachen <jiachen247@gmail.com>
2018-02-24 10:31:22 +0800

    refractor

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import os
 
-
-from telegram.ext import Updater
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import escape_markdown
 
-
-BOT_TOKEN = os.environ.get('BOT_TOKEN') # remember we set this earlier :)
-APP_NAME = os.environ.get('HEROKU_APP_NAME') # remember we set this earlier :)
+BOT_TOKEN = os.environ.get('BOT_TOKEN')  # remember we set this earlier :)
+APP_NAME = os.environ.get('HEROKU_APP_NAME')  # remember we set this earlier :)
 BOT_NAME = "@blahblahblah"
 BOT_NAME_ESCAPED = escape_markdown(BOT_NAME)
 
-PORT = int(os.environ.get('PORT', '80')) # this is set by heroku
+PORT = int(os.environ.get('PORT', '80'))  # this is set by heroku
 
 WEBHOOK_PATH = "hook{}".format(BOT_TOKEN)
-WEBHOOK_URL ="https://{}.herokuapp.com/{}".format(APP_NAME, WEBHOOK_PATH)
+WEBHOOK_URL = "https://{}.herokuapp.com/{}".format(APP_NAME, WEBHOOK_PATH)
+
 
 def start(bot, update):
     user_name = update.effective_user.name
@@ -25,43 +23,41 @@ def help(bot, update):
     update.message.reply_text('this is the list of commands i support!!! \n...')
 
 
-def handleTextMessage(bot, update):
+def handle_text_message(bot, update):
+    text = update.message.text
 
-	text = update.message.text
-    
-	
-	if "hello" in text.lower():
-		print("daww user is trying to say hello to me!!!")
-		
-		reply = "HELLO THERE! MY NAME IS *{}*".format(BOT_NAME_ESCAPED)
-		update.message.reply_text(reply, parse_mode="Markdown")
+    if "hello" in text.lower():
+        print("daww user is trying to say hello to me!!!")
 
-	elif text == "something else":
-		print("do something elseeeeee")
-		pass
-	
-	else:
-		print("REPEAT WHAT EVER THE USER SAID")
-		update.message.reply_text(text)
+        reply = "HELLO THERE! MY NAME IS *{}*".format(BOT_NAME_ESCAPED)
+        update.message.reply_text(reply, parse_mode="Markdown")
+
+    elif text == "something else":
+        print("do something elseeeeee")
+        pass
+
+    else:
+        print("REPEAT WHAT EVER THE USER SAID")
+        update.message.reply_text(text)
+
 
 def init():
+    updater = Updater(BOT_TOKEN)
 
-	updater = Updater(BOT_TOKEN)
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=WEBHOOK_PATH)
 
-	updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=WEBHOOK_PATH)
+    updater.bot.set_webhook(WEBHOOK_URL)
 
-	updater.bot.set_webhook(WEBHOOK_URL)
+    dp = updater.dispatcher
 
-	dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, handle_text_message))
 
-	dp.add_handler(CommandHandler("start", start ))
-	dp.add_handler(CommandHandler("help", help))
-	dp.add_handler(MessageHandler(Filters.text, handleTextMessage))
-
-	updater.idle()
+    updater.idle()
 
 
 if __name__ == '__main__':
-	init()
+    init()
